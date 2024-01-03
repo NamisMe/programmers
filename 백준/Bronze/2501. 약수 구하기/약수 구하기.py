dba720f9bb65af30b8495abc1c33b44a3d064ca0
NamisMe <88610815+NamisMe@users.noreply.github.com>
2024-01-03 14:07:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+
+factors = []
+for i in range(1, N+1):
+  if N % i == 0:
+    factors.append(i)
+
+if len(factors) < K :
+  print(0)
+else:
+  print(factors[K-1])
